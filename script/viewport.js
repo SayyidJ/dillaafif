841e9dfda499d0355c86dded3b006f44fb2b1c9a
SayyidJ <sayyidjee@gmail.com>
2023-07-15 17:27:36 +0700

    fix

--- script/viewport.js
+++
@@ -1,8 +0,0 @@
-const perbaikiTinggi = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-  console.log(`change app height: ${window.innerHeight}`);
-};
-
-window.addEventListener("resize", perbaikiTinggi);
-perbaikiTinggi();
